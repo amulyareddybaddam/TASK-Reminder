8086330897e02b1697d83b5d285bba68484d37e2
amulyareddybaddam <amulya555reddy@gmail.com>
2021-10-29 19:04:21 -0500

    Updated AlarmService

--- app/src/main/java/com/example/taskreminder/AlarmService.java
+++ app/src/main/java/com/example/taskreminder/AlarmService.java
@@ -1,86 +1,104 @@
 package com.example.taskreminder;
 
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.recyclerview.widget.GridLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
-
-
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.app.Service;
+import android.content.Context;
 import android.content.Intent;
-import android.os.Bundle;
-import android.util.Log;
-import android.view.Menu;
+import android.graphics.Color;
+import android.media.MediaPlayer;
+import android.os.Build;
+import android.os.IBinder;
+import android.os.Vibrator;
 
-import android.view.MenuItem;
-import android.view.View;
-import android.widget.Button;
-
-import android.widget.Toast;
-
-import java.util.ArrayList;
+import androidx.annotation.Nullable;
+import androidx.annotation.RequiresApi;
+import androidx.core.app.NotificationCompat;
 
 
-public class MainActivity extends AppCompatActivity {
-    DBHelper helper = new DBHelper(this);
-    Button createTask;
-    RecyclerView recyclerView;
-    ArrayList<Alarm> arr = new ArrayList<>();
-    MyAdapter adapter;
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        String t = null;
-        if(t==null){
-            Log.d("str45","is empty");}
-
-        createTask = findViewById(R.id.taskBTN);
-        recyclerView = findViewById(R.id.taskList);
-        loadData();
-
-        createTask.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent editIntent = new Intent(MainActivity.this, EditActivity.class);
-                startActivity(editIntent);
-
-            }
-        });
-    }
-    public void loadData() {
+import static com.example.taskreminder.AlarmBroadcastReceiver.TITLE;
 
 
-        arr = helper.getAllData();
-        recyclerView.setLayoutManager(new GridLayoutManager(this, 1));
-        adapter = new MyAdapter(this, arr);
-        recyclerView.setAdapter(adapter);
-    }
+public class AlarmService extends Service {
+    private MediaPlayer mediaPlayer;
+    private Vibrator vibrator;
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.nav_menu, menu);
-        return true;
+    public void onCreate() {
+        super.onCreate();
+
+        mediaPlayer = MediaPlayer.create(this, R.raw.alarm);
+        mediaPlayer.setLooping(true);
+
+        vibrator = (Vibrator) getSystemService(Context.VIBRATOR_SERVICE);
+
     }
+
+    @RequiresApi(api = Build.VERSION_CODES.O)
     @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        Toast.makeText(this, "Selected Item: " +item.getTitle(), Toast.LENGTH_SHORT).show();
-        switch (item.getItemId()) {
-            case R.id.help:
-                Intent helpint = new Intent(MainActivity.this, HelpFragment.class);
-                startActivity(helpint);
-                return true;
-            case R.id.dashboard:
-                Intent boardint = new Intent(MainActivity.this, MainActivity.class);
-                startActivity(boardint);
-                return true;
-            case R.id.rateUs:
-                Intent rateint= new Intent(MainActivity.this, rateUsActivity.class);
-                startActivity(rateint);
-                return true;
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        String Title = intent.getStringExtra(TITLE);
 
-            default:
-                return super.onOptionsItemSelected(item);
-        }
+
+        if(Title==null) Title = "Snoozed";
+        String alarmTitle = String.format("%s reminder (click me)", Title);
+        Intent notificationIntent = new Intent(this, Ringing.class);
+
+        notificationIntent.putExtra("title",Title);
+
+        notificationIntent.setAction(Long.toString(System.currentTimeMillis()));
+
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, notificationIntent, PendingIntent.FLAG_UPDATE_CURRENT);
+
+
+
+
+
+        String NOTIFICATION_CHANNEL_ID = "com.example.alaramclock";
+        String channelName = "My Background Service";
+        NotificationChannel chan = new NotificationChannel(NOTIFICATION_CHANNEL_ID, channelName, NotificationManager.IMPORTANCE_HIGH);
+        chan.setLightColor(Color.BLUE);
+        chan.setLockscreenVisibility(Notification.VISIBILITY_PRIVATE);
+        NotificationManager manager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+        assert manager != null;
+        manager.createNotificationChannel(chan);
+
+        NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder(this, NOTIFICATION_CHANNEL_ID);
+        Notification notification = notificationBuilder.setOngoing(true)
+                .setSmallIcon(R.drawable.ic_baseline_alarm)
+                .setContentTitle(alarmTitle)
+                .setPriority(NotificationManager.IMPORTANCE_MIN)
+                .setContentIntent(pendingIntent)
+                .setCategory(Notification.CATEGORY_SERVICE)
+                .build();
+        startForeground(2, notification);
+
+        mediaPlayer.start();
+
+        long[] pattern = { 0, 100, 1000 };
+        vibrator.vibrate(pattern, 0);
+
+        startForeground(1, notification);
+
+
+        return START_STICKY;
     }
 
 
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+
+        mediaPlayer.stop();
+        vibrator.cancel();
+    }
+
+    @Nullable
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
 }
