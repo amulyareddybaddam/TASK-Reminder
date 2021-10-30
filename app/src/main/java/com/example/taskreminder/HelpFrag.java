e8f0d860333660eab4f6386ed9e82a39f3d671c2
Vivek Tadiparthi <83790881+vivektadiparthi@users.noreply.github.com>
2021-10-29 19:40:20 -0500

    Added HelpFrag

---
+++ app/src/main/java/com/example/taskreminder/HelpFrag.java
@@ -0,0 +1,17 @@
+package com.example.taskreminder;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+
+import android.os.Bundle;
+
+public class HelpFrag extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.fragment_help);
+
+
+    }
+}
